52bcffd8076387975241ee3957a5c37c8f20a3d4
mdallamaiqbal <bd999plus13@gmail.com>
2026-02-26 12:17:22 +0600

    template string

---
+++ templateString.js
@@ -0,0 +1,8 @@
+//template string;
+/* template string variable */
+const firstName='Abul';
+const lastName='kalam';
+/* const fullName=firstName+' '+lastName;
+console.log('Hi my name is',fullName); */
+const fullName=`Hi my name is ${firstName} ${lastName}`;
+console.log(fullName);
